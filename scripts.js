c1faaaafdb74b371fda4ed42d2e2e5911bd5c1d7
TarrynH <Tarrynhurrell@gmail.com>
2023-07-05 18:21:24 +0200

    changes

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ const leo = {
 }
 
 const sarah = {
-	name: sarahName.trim() + " " + sarahSurname,
+	name: sarahName.trim() + " " + sarahSurname,  // or trim sarahName before (to sarahTrimmed) and do `${sarahTrimmed} ${sarahSurname}`
 	age: 62,
 	"access-id": "6b279ae5-5657-4240-80e9-23f6b635f7a8",
 	balance: sarahBalance*-1,
